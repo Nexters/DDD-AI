95c575956e050ab5e52f898554782d3cbb12034c
HyoTaek-Jang <hyotaek9812@gmail.com>
2025-01-18 01:00:11 +0900

    [DDD-1] add api structure

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI, Response
 
+from dto.request_dto import ChatCommonRequest
+
 app = FastAPI()
+
 
 @app.get("/health_check")
 def health_check():
     return Response(status_code=200)
+
+
+@app.post("/api/v1/classify/chat")
+def classify_chat(req: ChatCommonRequest):
+    pass
+
 
 if __name__ == "__main__":
     import uvicorn
