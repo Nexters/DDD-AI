1be591cde511224b6510541d7b70bd9aee1daab0
gibeom <lgb@paytalab.com>
2025-01-23 00:47:46 +0900

    [DDD-14] fix: delete question request field

--- main.py
+++ main.py
@@ -40,7 +40,8 @@ def reply_general_chat(req: ChatCommonRequest):
 @app.post("/api/v1/reply/tarot-chat")
 def reply_tarot_chat(req: ChatWithTarotCardCommonRequest):
     return llm_reply_tarot_chat(
-        question=req.chat,
+        # TODO 논의 필요
+        # question=req.chat,
         chat_room_id=req.chat_room_id,
         tarot_card=req.tarot_card
     )
