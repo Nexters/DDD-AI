8b451bf8e8477d655bbd804c7443ffbe6ec5393e
HyoTaek-Jang <hyotaek9812@gmail.com>
2025-01-29 18:39:46 +0900

    [DDD-22] add chat graph

--- dto/response_dto.py
+++ dto/response_dto.py
@@ -4,6 +4,13 @@ from pydantic import BaseModel
 class InternalErrorResponse(BaseModel):
     answer: str
 
-    def __init__(self, answer: str="헉!! 답변하는 과정에서 오류가 발생했다냥😿 미안하다냥...🙀"):
-        self.answer = answer
-        super().__init__()
+    def __init__(self, answer: str = "헉!! 답변하는 과정에서 오류가 발생했다냥😿 미안하다냥...🙀"):
+        super().__init__(answer=answer)
+
+
+class ChatGraphResponse(BaseModel):
+    classification: str
+    answer: str
+
+    def __init__(self, classification: str, answer: str):
+        super().__init__(classification=classification, answer=answer)
